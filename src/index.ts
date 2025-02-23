bc32b696be9f097741c09ad49fbc52f94013e87c
Anthony Fu <github@antfu.me>
2025-02-23 13:54:59 +0800

    feat: improve types

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ export interface QuansyncInputObject<Args extends any[], Return> {
   async: (...args: Args) => Promise<Return>
 }
 export type QuansyncInputGenerator<Args extends any[], Return>
-  = ((...args: Args) => Generator<QuansyncYield<any>, Return, unknown>)
+  = ((...args: Args) => QuansyncGenerator<Return>)
+
+export type QuansyncInput<Args extends any[], Return> =
+  | QuansyncInputObject<Args, Return>
+  | QuansyncInputGenerator<Args, Return>
 
 export interface QuansyncYield<R> {
   name?: string
@@ -13,11 +17,13 @@ export interface QuansyncYield<R> {
   __isQuansync: true
 }
 
-export type QuansyncGenerator<Return = any, Yield = any> =
-  Generator<QuansyncYield<Yield>, Return, Yield>
+export type UnwrapQuansyncReturn<T> = T extends QuansyncYield<infer R> ? R : T
+
+export type QuansyncGenerator<Return = any, Yield = unknown> =
+  Generator<Yield, Return, UnwrapQuansyncReturn<Yield>>
 
 /**
- * "Superposition" between async and sync.
+ * "Superposition" function that can be consumed in both sync and async contexts.
  */
 export type QuansyncFn<Args extends any[] = [], Return = any> =
   ((...args: Args) => QuansyncGenerator<Return>)
@@ -33,7 +39,7 @@ export function isQuansyncYield<T>(value: any | QuansyncYield<T>): value is Quan
 function quansyncObject<Args extends any[], Return>(
   options: QuansyncInputObject<Args, Return>,
 ): QuansyncFn<Args, Return> {
-  const generator = function *(...args: Args): QuansyncGenerator<Return> {
+  const generator = function *(...args: Args): QuansyncGenerator<Return, any> {
     return yield {
       name: options.name,
       sync: () => options.sync(...args),
@@ -48,35 +54,37 @@ function quansyncObject<Args extends any[], Return>(
   return generator
 }
 
+function unwrapSync(value: any): any {
+  return isQuansyncYield(value)
+    ? value.sync()
+    : value
+}
+
+function unwrapAsync(value: any): Promise<any> {
+  return isQuansyncYield(value)
+    ? value.async()
+    : Promise.resolve(value)
+}
+
 function quansyncGenerator<Args extends any[], Return>(
   generator: QuansyncInputGenerator<Args, Return>,
 ): QuansyncFn<Args, Return> {
   function sync(...args: Args): Return {
-    const unwrap = (value: any): any => {
-      return isQuansyncYield(value)
-        ? value.sync()
-        : value
-    }
     const iterator = generator(...args)
     let current = iterator.next()
     while (!current.done) {
-      current = iterator.next(unwrap(current.value))
+      current = iterator.next(unwrapSync(current.value))
     }
-    return unwrap(current.value)
+    return unwrapSync(current.value)
   }
 
   async function async(...args: Args): Promise<Return> {
-    const unwrap = (value: any): any => {
-      return isQuansyncYield(value)
-        ? value.async()
-        : value
-    }
     const iterator = generator(...args)
     let current = iterator.next()
     while (!current.done) {
-      current = iterator.next(await unwrap(current.value))
+      current = iterator.next(await unwrapAsync(current.value))
     }
-    return await unwrap(current.value)
+    return await unwrapAsync(current.value)
   }
 
   return quansyncObject({
@@ -86,14 +94,11 @@ function quansyncGenerator<Args extends any[], Return>(
   })
 }
 
+/**
+ * Creates a new Quansync function, a "superposition" between async and sync.
+ */
 export function quansync<Args extends any[], Return>(
-  options: QuansyncInputObject<Args, Return>,
-): QuansyncFn<Args, Return>
-export function quansync<Args extends any[], Return>(
-  options: QuansyncInputGenerator<Args, Return>,
-): QuansyncFn<Args, Return>
-export function quansync<Args extends any[], Return>(
-  options: QuansyncInputObject<Args, Return> | QuansyncInputGenerator<Args, Return>,
+  options: QuansyncInput<Args, Return>,
 ): QuansyncFn<Args, Return> {
   if (typeof options === 'function')
     return quansyncGenerator(options)
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,60 +1,57 @@
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 import { isQuansyncYield, quansync } from '../src'
 
-describe('should', () => {
-  it('asyncObject', async () => {
-    const add = quansync({
-      name: 'add',
-      sync: (a: number, b: number) => a + b,
-      async: async (a: number, b: number) => {
-        await new Promise(resolve => setTimeout(resolve, 10))
-        return a + b
-      },
-    })
-
-    const result1 = add(1, 2).next().value
-    if (isQuansyncYield(result1)) {
-      expect(result1.sync()).toBe(3)
-      expect(await result1.async()).toBe(3)
-    }
-    else {
-      throw new Error('result1 is not QuansyncYield')
-    }
-
-    expect(add.sync(4, 5)).toBe(9)
-    expect(await add.async(4, 5)).toBe(9)
+it('basic', async () => {
+  const add = quansync({
+    name: 'add',
+    sync: (a: number, b: number) => a + b,
+    async: async (a: number, b: number) => {
+      await new Promise(resolve => setTimeout(resolve, 10))
+      return a + b
+    },
   })
 
-  it('asyncGenerator', async () => {
-    const add = quansync({
-      name: 'add',
-      sync: (a: number, b: number) => a + b,
-      async: async (a: number, b: number) => {
-        await new Promise(resolve => setTimeout(resolve, 10))
-        return a + b
-      },
-    })
+  const result1 = add(1, 2).next().value
+  if (isQuansyncYield(result1)) {
+    expect(result1.sync()).toBe(3)
+    expect(await result1.async()).toBe(3)
+  }
+  else {
+    throw new Error('result1 is not QuansyncYield')
+  }
 
-    const toString = quansync({
-      name: 'toString',
-      sync: (value: any) => String(value),
-      async: async (value: any) => {
-        await new Promise(resolve => setTimeout(resolve, 10))
-        return String(value)
-      },
-    })
+  expect(add.sync(4, 5)).toBe(9)
+  expect(await add.async(4, 5)).toBe(9)
+})
 
-    const multiply = quansync(function *(a: number, b: number) {
-      let sum = 0
-      for (let i = 0; i < b; i++) {
-        const value = yield * add(sum, a)
-        sum = value
-      }
-      const str = yield * toString(sum)
-      return str
-    })
-
-    expect(multiply.sync(2, 3)).toBe('6')
-    expect(await multiply.async(4, 5)).toBe('20')
+it('generator', async () => {
+  const add = quansync({
+    name: 'add',
+    sync: (a: number, b: number) => a + b,
+    async: async (a: number, b: number) => {
+      await new Promise(resolve => setTimeout(resolve, 10))
+      return a + b
+    },
   })
+
+  const toString = quansync({
+    name: 'toString',
+    sync: (value: any) => String(value),
+    async: async (value: any) => {
+      await new Promise(resolve => setTimeout(resolve, 10))
+      return String(value)
+    },
+  })
+
+  const multiply = quansync(function *(a: number, b: number) {
+    let sum = (yield 0) as number
+    for (let i = 0; i < b; i++) {
+      const value = yield * add(sum, a)
+      sum = value
+    }
+    return yield * toString(sum)
+  })
+
+  expect(multiply.sync(2, 3)).toBe('6')
+  expect(await multiply.async(4, 5)).toBe('20')
 })
