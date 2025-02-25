ac891250a54167471018466f0bac7a6e9f301932
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-26 07:30:01 +0800

    fix

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,38 +1,54 @@
-/* eslint-disable no-unused-expressions */
-
+// @ts-check
 import gensync from 'gensync'
-import { Bench } from 'tinybench'
+import { Bench, hrtimeNow } from 'tinybench'
 // eslint-disable-next-line antfu/no-import-dist
 import { quansync } from '../dist/index.mjs'
 
 const bench = new Bench({
-  name: 'quansync benchmark',
+  name: 'sync benchmark',
   time: 100,
+  now: hrtimeNow,
+})
+
+const fnSync = () => 10
+const fnAsync = async () => 10
+const quansyncFn = quansync({
+  sync: fnSync,
+  async: fnAsync,
+})
+const gensyncFn = gensync({
+  sync: fnSync,
+  async: fnAsync,
+})
+
+const addNativeSync = /** @param {number} n */ n => fnSync() + n
+const addNativeAsync = /** @param {number} n */ async n => await fnAsync() + n
+
+const quansyncAdd = quansync(/** @param {number} n */ function* (n) {
+  return (yield * quansyncFn()) + n
+})
+const gensyncAdd = gensync(/** @param {number} n */ function* (n) {
+  return (yield * gensyncFn()) + n
 })
 
 bench
-  .add('native', async () => {
-    const fnSync = () => 10
-    const fnAsync = async () => 10
-
-    fnSync() + 1;
-    (await fnAsync()) + 1
+  .add('sync: native', () => {
+    addNativeSync(1)
   })
-  .add('quansync', async () => {
-    const fn = quansync({
-      sync: () => 10,
-      async: async () => 10,
-    })
-    fn.sync() + 1;
-    (await fn.async()) + 1
+  .add('sync: quansync', () => {
+    quansyncAdd.sync(1)
   })
-  .add('gensync', async () => {
-    const fn = gensync({
-      sync: () => 10,
-      async: async () => 10,
-    })
-    fn.sync();
-    (await fn.async()) + 1
+  .add('sync: gensync', async () => {
+    gensyncAdd.sync(1)
+  })
+  .add('async: native', async () => {
+    await addNativeAsync(1)
+  })
+  .add('async: quansync', async () => {
+    await quansyncAdd.async(1)
+  })
+  .add('async: gensync', async () => {
+    await gensyncAdd.async(1)
   })
 
 console.info('Running benchmark...')
