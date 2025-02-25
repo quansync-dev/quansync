984bfaa3cd33286b9a847ccd45fb6d8a4d849eb9
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-26 07:43:21 +0800

    update

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -10,44 +10,66 @@ const bench = new Bench({
   now: hrtimeNow,
 })
 
-const fnSync = () => 10
-const fnAsync = async () => 10
+const sync = () => 10
+const async = async () => 10
+const addNativeSync = /** @param {number} n */ n => sync() + n
+const addNativeAsync = /** @param {number} n */ async n => await async() + n
+
 const quansyncFn = quansync({
-  sync: fnSync,
-  async: fnAsync,
+  sync: () => 10,
+  async: async () => 10,
 })
-const gensyncFn = gensync({
-  sync: fnSync,
-  async: fnAsync,
-})
-
-const addNativeSync = /** @param {number} n */ n => fnSync() + n
-const addNativeAsync = /** @param {number} n */ async n => await fnAsync() + n
-
 const quansyncAdd = quansync(/** @param {number} n */ function* (n) {
   return (yield * quansyncFn()) + n
+})
+
+const gensyncFn = gensync({
+  sync: () => 10,
+  async: async () => 10,
 })
 const gensyncAdd = gensync(/** @param {number} n */ function* (n) {
   return (yield * gensyncFn()) + n
 })
 
 bench
-  .add('sync: native', () => {
+  .add('sync: native fn', () => {
+    sync()
+  })
+  .add('sync: quansync fn', () => {
+    quansyncFn.sync()
+  })
+  .add('sync: gensync fn ', () => {
+    gensyncFn.sync()
+  })
+
+  .add('sync: native add', () => {
     addNativeSync(1)
   })
-  .add('sync: quansync', () => {
+  .add('sync: quansync add', () => {
     quansyncAdd.sync(1)
   })
-  .add('sync: gensync', async () => {
+  .add('sync: gensync add', () => {
     gensyncAdd.sync(1)
   })
-  .add('async: native', async () => {
+
+  // ASYNC
+  .add('async: native fn', async () => {
+    await async()
+  })
+  .add('async: quansync fn', async () => {
+    await quansyncFn.async()
+  })
+  .add('async: gensync fn ', async () => {
+    await gensyncFn.async()
+  })
+
+  .add('async: native add', async () => {
     await addNativeAsync(1)
   })
-  .add('async: quansync', async () => {
+  .add('async: quansync add', async () => {
     await quansyncAdd.async(1)
   })
-  .add('async: gensync', async () => {
+  .add('async: gensync add', async () => {
     await gensyncAdd.async(1)
   })
 
