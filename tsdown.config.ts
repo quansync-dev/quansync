abf2b51ef57b97b7e217eeb099e8c669f122416c
Kevin Deng <sxzz@sxzz.moe>
2025-12-03 05:31:15 +0800

    feat!: drop `quansync/types` entry

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: 'src/*',
+  entry: 'src/{index,macro}.ts',
   format: ['cjs', 'esm'],
   inlineOnly: [],
   exports: true,
